562e2b9e9f9f57b97c29d98d0d82fb22a8fbd8fd
Kyle Bartolucci <bartoluccik@gmail.com>
2020-09-20 18:04:41 -0700

    added react-router-dom to project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+import { Route } from 'react-router-dom'
+
+const Hat = () => (
+  <h1>Hats</h1>
+)
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Route exact path='/' component={ HomePage } />
+      <Route exact path='/hats' component={ Hat } />
     </div>
   );
 }
